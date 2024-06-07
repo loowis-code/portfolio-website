f061cb1b94af74fe8cf89f202fe72f6f0ba995b5
Lewis Inches <incheslewis@gmail.com>
2024-06-07 23:09:27 +0100

    add projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -3,31 +3,24 @@ import styles from '@/components/css-modules/projects.module.css';
 export default function Projects () {
 
     return (
-        <div className={styles.main}>
-            <h2 className={styles.header}>Projects</h2>
-
-            <div className={`card mb-3 shadow-lg ${styles.card}`}>
-            <img src="/photography_website.png" className="card-img-top" alt="Screenshot preview of photography website"></img>
-
-                <div className="card-body">
-                    <h3 className={styles.header}>Photography Website</h3>
-                    <p className="card-text">
-                    A website used to display my photography, built using Next.js and FaunaDB.
-                    </p>
-                    <a className={styles.link} href="https://www.lewisinches.pictures">Go to Project</a>
-                    <a className={styles.link} href="https://github.com/LewisI224/photography-website">Go to Code</a>
+        <div className={styles.card}>
+            <div>
+                <h2>Projects</h2>
+                <div>
+                    <div className={styles.imageWrapper}>
+                        <img className={styles.image} src="/photography_website.png" alt="Screenshot preview of photography website"></img>
+                    </div>
+                    
+                    <div>
+                        <h3>Photography Website</h3>
+                        <p>
+                            A website used to display my photography, built using Next.js and FaunaDB.
+                        </p>
+                        <a href="https://www.lewisinches.pictures">Go to Project</a>
+                        <a href="https://github.com/LewisI224/photography-website">Go to Code</a>
+                    </div>
                 </div>
             </div>
-
-            {/* <div className={`card mb-3 shadow-lg ${styles.card}`}>
-                <div className="card-body">
-                    <h3 className={styles.header}>Portfolio Website</h3>
-                    <p className="card-text">
-                    A personal portfolio website built using Next.js with TypeScript.
-                    </p>
-                </div>
-            </div> */}
-
         </div>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Layout from '@/components/layout';
 import Introduction from '@/components/introduction';
 import AboutMe from '@/components/aboutme';
+import Projects from '@/components/projects';
 
 function Home() {
     return (
         <Layout>
             <Introduction/>
             <AboutMe/>
+            <Projects/>
         </Layout>
     )
 }
