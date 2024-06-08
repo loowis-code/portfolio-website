7675493d5ead1698258c20c53cc81921c517135f
Lewis Inches <incheslewis@gmail.com>
2024-06-08 21:45:01 +0100

    layout and styling changes

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,19 @@
 import styles from '@/components/css-modules/header.module.css';
+import { useEffect } from 'react';
 
 export default function Header () {
+
+
+    useEffect(() => {
+        window.addEventListener('wheel', function() {
+            const header = document.querySelector<HTMLElement>(`.${styles.header}`);
+            if (window.scrollY < 150 && header !== null) {
+                header.style.opacity = '1';
+            } else if (header !== null){
+                header.style.opacity = '0.5';
+            }
+        });
+    }, []);
 
     return (
         <div className={styles.header}>
@@ -12,6 +25,7 @@ export default function Header () {
             <div className={styles.pages}>
                 <p className={styles.page} tabIndex={0}>about me</p>
                 <p className={styles.page} tabIndex={0}>projects</p>
+                <p className={styles.page} tabIndex={0}>résumé 🔗</p>
                 <p className={styles.page} tabIndex={0}>contact</p>
             </div>
         </div>
