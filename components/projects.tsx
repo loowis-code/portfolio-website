5fb22ba0d79728d56006302374907df0fb93c8b6
Lewis Inches <incheslewis@gmail.com>
2024-12-01 21:52:47 +0000

    projects redesign

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,44 +1,44 @@
 import styles from '@/components/css-modules/projects.module.css';
+import Image from 'next/image';
 
 export default function Projects () {
 
     return (
         <div className={styles.card}>
-                <h2 className={styles.header} tabIndex={0} id={"projects"}>Projects</h2>
-                <div className={styles.body}>
-                    <div className={styles.information}>
-                        <h3 tabIndex={0}>Photography Website</h3>
-                        <p className={styles.description} tabIndex={0}>
-                            A website used to display my photography, built using Next.js and FaunaDB.
-                        </p>
-                        <div className={styles.links}>
-                            <a tabIndex={0} className={styles.link} href="https://www.lewisinches.pictures">Website Link</a>
-                            <a tabIndex={0} className={styles.link} href="https://github.com/LewisI224/photography-website">Project on Github</a>
-                        </div>
-                    </div>
-                    
-                    <div className={styles.imageWrapperR}>
-                        <img tabIndex={0} className={styles.image} src="/photography_website.png" alt="Screenshot preview of photography website"></img>
-                    </div>
+            <h2 className={styles.header} tabIndex={0} id={"projects"}>Projects</h2>
+            <div className={styles.project}>
+                <div className={styles.info}>
+                    <h3 tabIndex={0}>Photography Portfolio</h3>
+                    <p tabIndex={0}>
+                        My photography portfolio website, built using Next.js and FaunaDB. 
+                    </p>
                 </div>
-
-                <div className={styles.body}>
-                    <div className={styles.imageWrapperL}>
-                        <img tabIndex={0} className={styles.image} src="/ez-insulin.png" alt="Screenshot preview of photography website"></img>
+                <div className={styles.polaroid}>
+                    <Image src="/photography_website.png" alt="Photography Portfolio" width={400} height={200} className={styles.image} />
+                    <div className={styles.links}>
+                        <a  className={styles.link} href=''>Website Link</a>
+                        <a  className={styles.link} href=''>Github Link</a>
                     </div>
-                    <div className={styles.information}>
-                        <h3 tabIndex={0}>Insulin Calculator</h3>
-                        <p className={styles.description} tabIndex={0}>
-                            A simple starter app to calulate insulin dosages based on current blood sugar readings and estimated carbohydrate content.
-                        </p>
-                        <div className={styles.links}>
-                            <a tabIndex={0} className={styles.link} href="/ez-insulin.sdk">Download SDK 🔗</a>
-                            <a tabIndex={0} className={styles.link} href="https://github.com/LewisI224/EzInsulin">Project on Github</a>
-                        </div>
-                    </div>
-                    
 
                 </div>
+            </div>
+
+            <div className={styles.project}>
+                <div className={styles.info}>
+                    <h3 tabIndex={0}>Insulin Calculator</h3>
+                    <p tabIndex={0}>
+                        My photography portfolio website, built using Next.js and FaunaDB. 
+                    </p>
+                </div>
+                <div className={styles.polaroid}>
+                    <Image src="/ez-insulin.png" alt="Photography Portfolio" width={108} height={240} className={styles.image}/>
+                    <div className={styles.links}>
+                        <a  className={styles.link} href=''>Website Link</a>
+                        <a  className={styles.link} href=''>Github Link</a>
+                    </div>
+
+                </div>
+            </div>
 
         </div>
     );
