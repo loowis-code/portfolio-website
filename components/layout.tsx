a0a4c3010eaf82ad1c188afba631d9c2591b3672
loowis <incheslewis@gmail.com>
2025-03-02 21:52:37 +0000

    impelement global header (#70)

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Header from '@/components/header'
+import { Header } from 'loowis-component-library';
 import styles from '@/components/css-modules/layout.module.css';
 
 type LayoutProps = React.PropsWithChildren<{}>;
@@ -13,7 +13,7 @@ export default function Layout({ children }: LayoutProps) {
             <meta name="description" content="Hi, My name is Lewis Inches and I'm a software engineer from Edinburgh. I currently work as a fullstack web developer at Skyscanner in Edinburgh." />
             </Head>
 
-            <Header />
+            <Header navTabs={['HOME', 'ABOUT ME', 'PROJECTS']} navLinks={['', '#aboutme', '#projects']}/>
 
             {children}
 
